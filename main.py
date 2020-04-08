36e3b458bc57b5176da785f424b5905554a63df8
pkyriakis <pvkiriakis@gmail.com>
2020-04-08 01:12:59 -0700

    codded persistence images vectorization by mistake; save it in case needed later

--- main.py
+++ main.py
@@ -4,11 +4,12 @@
 
 from persistence_diagram import PDiagram
 import utils
-
 train_images, train_labels, test_images, test_labels = utils.get_mnist_data()
 
-pd = PDiagram(train_images[0:10,:,:], images_id= 'mnist_train',save_every=100)
-dgms_dict = pd.get_peristence_diagrams()
+#pd = PDiagram(train_images, images_id= 'mnist_train')
+#dgms_dict = pd.get_vectorized_pds()
 
-# pd = PDiagram(test_images, images_id= 'mnist_test',save_every=10)
-# dgms_dict = pd.get_peristence_diagrams()
+
+pd = PDiagram(test_images[0:1,:,:], images_id= 'mnist_test')
+embpnts = pd.get_embedded_pds(man_dim=9)
+print(embpnts[1])
