52d3468fd5c2dc17ababc70f7192bbe54dd77bdf
pkyriakis <pvkiriakis@gmail.com>
2020-04-13 14:02:39 -0700

    coded the part to extract PDs for different filtrations

--- persistence_diagram.py
+++ persistence_diagram.py
@@ -5,21 +5,24 @@ from scipy.stats import multivariate_normal
 import matplotlib.pyplot as plt
 from collections import defaultdict
 from time import sleep
+from mpl_toolkits.mplot3d import Axes3D
 
-import dionysus as dn
 import numpy as np
 import multiprocessing, random
 from scipy.integrate import simps, dblquad
 
+from gtda.images import *
+from gtda.homology import CubicalPersistence
+
 
 class PDiagram():
-    def __init__(self,images, images_id = 'mnist', num_of_fil = 500, man_dim = 9):
+    def __init__(self, images, fil_parms, images_id='mnist', num_of_fil=500, man_dim=9):
         self.images = images
         self.dgms_dir = 'data/dgms/' + images_id + "/"
         self.vdgms_dir = 'data/vdgms/' + images_id + "/"
         self.edgms_dir = 'data/edgms/' + images_id + "/"
+        self.fil_params = fil_parms
 
-        self.f_values = np.linspace(0, 1, num_of_fil)
         self.man_dim = man_dim
 
         self.num_images = images.shape[0]
@@ -30,31 +33,32 @@ class PDiagram():
         self.dgms_dict = self.manager.dict()
         self.vdgms_dict = self.manager.dict()
         self.edgms_dict = self.manager.dict()
+        self.done = multiprocessing.Value("i", len(self.dgms_dict))
 
-        if not os.path.exists(self.dgms_dir):
-            os.makedirs(self.dgms_dir)
-            os.makedirs(self.dgms_dir)
-            self.done = multiprocessing.Value("i", 0)
-        else:
-            self.dgms_dict = self.__load_pds(self.dgms_dir)
-            self.done = multiprocessing.Value("i", len(self.dgms_dict))
-
-        if not os.path.exists(self.vdgms_dir):
-            os.makedirs(self.vdgms_dir)
-        else:
-            self.vdgms_dict = self.__load_pds(self.vdgms_dir)
-
-        if not os.path.exists(self.edgms_dir):
-            os.makedirs(self.edgms_dir)
-        else:
-            self.edgms_dict = self.__load_pds(self.edgms_dir)
+        # if not os.path.exists(self.dgms_dir):
+        #     os.makedirs(self.dgms_dir)
+        #     os.makedirs(self.dgms_dir)
+        #     self.done = multiprocessing.Value("i", 0)
+        # else:
+        #     self.dgms_dict = self.__load_pds(self.dgms_dir)
+        #     self.done = multiprocessing.Value("i", len(self.dgms_dict))
+        #
+        # if not os.path.exists(self.vdgms_dir):
+        #     os.makedirs(self.vdgms_dir)
+        # else:
+        #     self.vdgms_dict = self.__load_pds(self.vdgms_dir)
+        #
+        # if not os.path.exists(self.edgms_dir):
+        #     os.makedirs(self.edgms_dir)
+        # else:
+        #     self.edgms_dict = self.__load_pds(self.edgms_dir)
 
     def __update_progress(self):
         '''
             Prints a progress
         '''
         while self.done.value <= self.num_images:
-            print('Done {}/{}'.format(self.done.value,self.num_images), end="\r")
+            print('Done {}/{}'.format(self.done.value, self.num_images), end="\r")
             if self.done.value < self.num_images:
                 sleep(10)
             else:
@@ -67,24 +71,22 @@ class PDiagram():
         '''
         dct = dict()
         for filename in os.listdir(dir):
-            with open(os.path.join(dir,filename), 'rb') as f:
+            with open(os.path.join(dir, filename), 'rb') as f:
                 input = np.load(f, allow_pickle=True)
                 for key in input.keys():
                     dct[int(key)] = input[key]
         return dct
-
 
     def __save_pds(self, dct, dir, chunk):
         '''
             Saves the given dict from start to end (not inclusive)
         :return:
         '''
-        dgms_dict_to_save = dict() # contains an entry for each image/diagram, keyed by the index of the image in self.images
+        dgms_dict_to_save = dict()  # contains an entry for each image/diagram, keyed by the index of the image in self.images
         for index in chunk:
             dgms_dict_to_save[str(index)] = dct[index]
-        rnd = random.randint(0,10000000)
+        rnd = random.randint(0, 10000000)
         np.savez(dir + "ckpnt_" + str(rnd), **dgms_dict_to_save)
-
 
     def __chunks(self, lst, n):
         '''
@@ -93,212 +95,152 @@ class PDiagram():
         for i in range(0, len(lst), n):
             yield lst[i:i + n]
 
-
-    def __pixel_to_vertices(self,i,j):
-        '''
-            Given the pixel it returns the vertices' ids of all 4 corner points
-        :param i: int
-        :param j: int
-        :return: list of ints
-        '''
-        points = [[i, j], [i + 1, j], [i, j + 1], [i + 1, j + 1]]
-        vertices = [k * (self.image_size + 1) + l for k, l in points]
-        return  vertices
-
-    def __vertex_to_pixels(self, vertex):
-        '''
-            Inverse of the above, given the vertex id it returns all the adjecent pixels
-        :param vertex:
-        :return:
-        '''
-        i = vertex // (self.image_size + 1)
-        j = vertex % (self.image_size + 1)
-
-        valid = lambda i,j : i in range(self.image_size) and j in range(self.image_size)
-        pixels = []
-        if valid(i,j):
-            pixels.append([i,j])
-        if valid(i-1,j):
-            pixels.append([i-1,j])
-        if valid(i,j-1):
-            pixels.append([i,j-1])
-        if valid(i-1,j-1):
-            pixels.append([i-1,j-1])
-        return pixels
-
-
     def __rotate_point(self, point):
         '''
-            Rotates a given point by 45deg to bring it to birth-persinstence form and scales down inf deaths if necessary
+            Rotates a given point by 45deg to bring it to birth-persinstence form
             @param: point - the point to be rotated
         '''
         birth = point[0]
         death = point[1]
-        death = np.min([death, np.max(self.f_values)])
-
         return [birth, death - birth]
-
-
-    def __get_complex_from_image(self, image):
-        '''
-            Obtains a simplicial complex from a given image
-        :param image: NxN @numpy.array
-        :return: simplicial complex of image: @dionysus.Simplex()
-        '''
-        complex = []
-        for i in range(self.image_size):
-            for j in range(self.image_size):
-                vertices = self.__pixel_to_vertices(i,j)
-                simplex = dn.Simplex(vertices)
-                complex.append(simplex)
-
-        return dn.closure(complex,2)
-
-
-    def __get_filtation_from_complex(self, image, complex):
-        '''
-            Assigns a real value to each simplex in the given complex
-            and returns the resulting sublevel set filtration
-        :param image:
-        :param complex:
-        :return:
-        '''
-        max_int = np.amax(image)
-        for simplex in complex:
-            min_intensity = max_int
-            list_vert = list(simplex)
-            pixels_all = []
-            for vertex in list_vert:
-                pixels_ver = []
-                for pixel in self.__vertex_to_pixels(vertex):
-                    pixels_ver.append(tuple(pixel))
-                pixels_all.append(pixels_ver)
-            pixels = set(pixels_all[0])
-            for pixel in pixels_all:
-                pixels.intersection(set(pixel))
-            for pixel in pixels:
-                i,j = list(pixel)
-                min_intensity = min(min_intensity, image[i,j])
-            simplex.data = min_intensity
-
-        filtration = dn.Filtration()
-        for value in self.f_values:
-            for simplex in complex:
-                if simplex.data <= value:
-                    filtration.append(simplex)
-        return filtration
 
     def __compute_pds_chunk(self, chunk):
         '''
-            Computes the PDs for the given chunk of images, start to end
-        :param start: int, starting index
-        :param end: int, last index
+            Computes the PDs for the given chunk of images using the params set in the self.fil_params
         :return:
         '''
+
         for index in chunk:
-            # Get the diagrams of the n-th image
-            image = self.images[index,:,:]
-            complex = self.__get_complex_from_image(image)
-            filtration = self.__get_filtation_from_complex(image, complex)
-            m = dn.homology_persistence(filtration)
-            dgms = dn.init_diagrams(m, filtration)
+            image = np.expand_dims(self.images[index], axis=0)
+            bin = Binarizer()
+            bin_image = bin.fit_transform(image)
+            dgm_image = dict()
+            for filtration in self.fil_params.keys():
+                params = self.fil_params[filtration]
+                cubical = CubicalPersistence(homology_dimensions=(0, 1))
+                if filtration == 'cubical':
+                    cub_dict = dict()
+                    cub_dict[0] = np.squeeze(cubical.fit_transform(image))
+                    dgm_image[filtration] = cub_dict
+                if filtration == 'height':
+                    height_dict = dict()
+                    for i in range(params.shape[0]):
+                        heigtht_fil = HeightFiltration(direction=params[i])
+                        filtered = heigtht_fil.fit_transform(bin_image)
+                        filtered = filtered / np.max(filtered)
+                        height_dict[i] = np.squeeze(cubical.fit_transform(filtered))
+                    dgm_image[filtration] = height_dict
+                if filtration == 'radial':
+                    radial_dict = dict()
+                    for i in range(params.shape[0]):
+                        radial_fil = RadialFiltration(center=params[i])
+                        filtered = radial_fil.fit_transform(bin_image)
+                        filtered = filtered / np.max(filtered)
+                        radial_dict[i] = np.squeeze(cubical.fit_transform(filtered))
+                    dgm_image[filtration] = radial_dict
+                if filtration == 'dilation':
+                    dil_dict = dict()
+                    for i in range(len(params)):
+                        dial_fil = DilationFiltration(n_iterations=params[i])
+                        filtered = dial_fil.fit_transform(bin_image)
+                        filtered = filtered / np.max(filtered)
+                        dil_dict[i] = np.squeeze(cubical.fit_transform(filtered))
+                    dgm_image[filtration] = dil_dict
+                if filtration == 'erosion':
+                    er_dict = dict()
+                    for i in range(len(params)):
+                        er_fil = ErosionFiltration(n_iterations=params[i])
+                        filtered = er_fil.fit_transform(bin_image)
+                        filtered = filtered / np.max(filtered)
+                        er_dict[i] = np.squeeze(cubical.fit_transform(filtered))
+                    dgm_image[filtration] = er_dict
 
-            # Convert it to list
-            dgm_id = 0
-            points = []
-            for dgm in dgms:
-                for point in dgm:
-                    birth = point.birth
-                    death = point.death
-                    death = np.min([death,np.max(self.f_values)])
-                    points.append([dgm_id, birth, death])
-                dgm_id = dgm_id + 1
+                # Store it in dict
+                self.dgms_dict[index] = dgm_image
+                self.done.value += 1
 
-            # Store it in dict
-            self.dgms_dict[index] = points
-            self.done.value += 1
-
-        self.__save_pds(self.dgms_dict, self.dgms_dir, chunk)
-
-
-    def __appr_integral(self,fun, x_low, x_up, y_low, y_up, K = 3):
+    def __appr_integral(self, fun, x_low, x_up, y_low, y_up, K=3):
         '''
             Approxiamates the 2d integral of the given function over a K-grid
         '''
-        x = np.linspace(x_low,x_up,K)
-        y = np.linspace(y_low,y_up,K)
-        xx, yy = np.meshgrid(x,y)
-        zz = np.zeros((K,K))
+        x = np.linspace(x_low, x_up, K)
+        y = np.linspace(y_low, y_up, K)
+        xx, yy = np.meshgrid(x, y)
+        zz = np.zeros((K, K))
         for i in range(K):
             for j in range(K):
-                zz[i,j] = fun(xx[i,j],yy[i,j])
-        res = simps(simps(zz,x),y)
+                zz[i, j] = fun(xx[i, j], yy[i, j])
+        res = simps(simps(zz, x), y)
         return res
-
 
     def __vectorize_pds_chunk(self, chunk):
         '''
             Vectorize the given chunk of PDs
         '''
-        cov = 0.1 * np.eye(2)
+        cov =  np.eye(2)
         phi = lambda x, y, mu: multivariate_normal(mean=mu, cov=cov).pdf([x, y])
-        grid_dim = int(np.sqrt(self.man_dim))
+        grid_dim = int(np.sqrt(self.man_dim) + 1)
 
         for index in chunk:
-            dgm = self.dgms_dict[index]
-            # Get PD for each homology class
-            dgm_of_hom = defaultdict(list)
-            for point in dgm:
-                hom = point[0]
-                birth, peristence = self.__rotate_point(point[1:])
-                dgm_of_hom[hom].append([birth, peristence])
+            dgms = self.dgms_dict[index]
+            vdgms = dict()
+            for filtration in dgms.keys():
+                vdgms_filtration = dict()
+                for ind in dgms[filtration].keys():
+                    dgm = dgms[filtration][ind]
+                    # Get PD for each homology class
+                    dgm_of_hom = defaultdict(list)
+                    for point in dgm:
+                        hom = point[-1]
+                        birth, peristence = self.__rotate_point(point[:2])
+                        dgm_of_hom[hom].append([birth, peristence])
 
-            # Init vectorization
-            num_of_hom = len(dgm_of_hom.keys())
-            vectorized_dgm = np.zeros([self.man_dim, num_of_hom])
+                    # Init vectorization
+                    num_of_hom = len(dgm_of_hom.keys())
+                    vectorized_dgm = np.zeros([self.man_dim, num_of_hom])
 
-            # Iterate over all homology classes
-            for hom in dgm_of_hom.keys():
-                # Get rid of dublicate points to speed up
-                unique_points = defaultdict(int)
-                for point in dgm_of_hom[hom]:
-                    tpnt = tuple(point)
-                    unique_points[tpnt] = unique_points[tpnt] + 1  # count multiplicity
+                    # Iterate over all homology classes
+                    for hom in dgm_of_hom.keys():
+                        # Get rid of dublicate points to speed up
+                        unique_points = defaultdict(int)
+                        for point in dgm_of_hom[hom]:
+                            tpnt = tuple(point)
+                            unique_points[tpnt] = unique_points[tpnt] + 1  # count multiplicity
 
-                rho = lambda x, y: \
-                     sum([mult * phi(x, y, list(mu)) for mu, mult in unique_points.items()])
+                        rho = lambda x, y: \
+                            sum([mult * phi(x, y, list(mu)) for mu, mult in unique_points.items()])
 
-                cnt = 0
-                lin_space = np.linspace(0, 1, grid_dim)
-                x_low = lin_space[0]
-                y_low = lin_space[0]
+                        cnt = 0
+                        lin_space = np.linspace(0, 1, grid_dim)
+                        x_low = lin_space[0]
+                        y_low = lin_space[0]
+                        # Compute vectorization
+                        lin_space = np.linspace(0, 1, grid_dim)
+                        mx, my = np.meshgrid(lin_space, lin_space)
+                        for i in range(grid_dim - 1):
+                            for j in range(grid_dim - 1):
+                                x_low = mx[i, j]
+                                x_up = mx[i, j + 1]
+                                y_low = my[i, j]
+                                y_up = my[i + 1, j]
+                                integral, error = dblquad(rho, y_low, y_up, x_low, x_up)
+                                vectorized_dgm[cnt, int(hom)] = integral
+                                cnt += 1
 
-                # Compute vectorization
-                for i in range(grid_dim):
-                    for j in range(grid_dim):
-                        x_up = lin_space[i]
-                        y_up = lin_space[j]
-                        vectorized_dgm[cnt, int(hom)] = rho(x_low, y_low) + rho(x_low, y_up) + rho(x_up, y_low) + rho(x_up, y_up)
-                        #integral = self.__appr_integral(rho, x_low, x_up, y_low, y_up)
-                        #integral, error = dblquad(rho, y_low, y_up, lambda x : x_low, lambda y : x_up, epsrel=0.01)
-                        #vectorized_dgm[cnt, int(hom)] = integral
-                        x_low = x_up
-                        y_low = y_up
-                        cnt += 1
-
+                    vdgms_filtration[ind] = vectorized_dgm
+                vdgms[filtration] = vdgms_filtration
             # Store it in dict
-            self.vdgms_dict[index] = vectorized_dgm
+            self.vdgms_dict[index] = vdgms
             self.done.value += 1
         self.__save_pds(self.vdgms_dict, self.vdgms_dir, chunk)
-
 
     def __embed_pds_chunk(self, chunk):
         '''
             Embeds points in PDs in an Euclidean space (for the given chunk of indices)
         '''
 
-        lin_space = np.linspace(0,1,int(np.sqrt(self.man_dim)))
-        grid_x,grid_y = np.meshgrid(lin_space,lin_space)
+        lin_space = np.linspace(0, 1, int(np.sqrt(self.man_dim)))
+        grid_x, grid_y = np.meshgrid(lin_space, lin_space)
         cov = np.eye(2)
         rho = lambda x, y, mu: multivariate_normal(mean=mu, cov=cov).pdf([x, y])
         for index in chunk:
@@ -308,7 +250,7 @@ class PDiagram():
             unique_points = defaultdict(int)
             for point in dgm:
                 if not tuple(point) in unique_points:
-                    unique_points[tuple(point)] = 0 # TODO need to figure out how to handle multiplicities
+                    unique_points[tuple(point)] = 0  # TODO need to figure out how to handle multiplicities
                     hom = point[0]
                     birth, peristence = self.__rotate_point(point[1:])
                     embeded_point = []
@@ -330,8 +272,8 @@ class PDiagram():
         '''
             Parellel run of target; the given list of indices creates the chunks to allocate to each cpu
         '''
-        cpus = 2*multiprocessing.cpu_count() # leave one cpu free
-        chuck_size = math.ceil(len(indices)/cpus)
+        cpus = 2 * multiprocessing.cpu_count()  # leave one cpu free
+        chuck_size = math.ceil(len(indices) / cpus)
 
         jobs = []
         for chunk in self.__chunks(indices, chuck_size):
@@ -341,9 +283,9 @@ class PDiagram():
             jobs.append(p)
 
         # Start a job to update progress bar
-        p = multiprocessing.Process(target=self.__update_progress)
-        jobs.append(p)
-        p.start()
+        # p = multiprocessing.Process(target=self.__update_progress)
+        # jobs.append(p)
+        # p.start()
 
         for job in jobs:
             job.join()
@@ -362,7 +304,6 @@ class PDiagram():
             self.__run_parallel(self.__compute_pds_chunk, inds_left)
 
         return self.dgms_dict
-
 
     def get_vectorized_pds(self):
         '''
@@ -393,13 +334,3 @@ class PDiagram():
             print('Embedding persistence diagrams...')
             self.__run_parallel(self.__embed_pds_chunk, inds_left)
         return self.edgms_dict
-
-
-
-
-
-
-
-
-
-
