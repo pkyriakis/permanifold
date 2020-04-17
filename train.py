46628bf46cfaaf99d66568b635bede94d1f2f629
pkyriakis <pvkiriakis@gmail.com>
2020-04-17 11:09:05 -0700

    coded layers.py using tf control flow

--- train.py
+++ train.py
@@ -1,20 +1,27 @@
 import datetime
 import os, math
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
 
 import persistence_diagram
 import tensorflow as tf
 import layers
 import numpy as np
 import utils
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-man_dim = 2 # Dimension of the manifold
-K = 28 # number of projection bases
-num_of_hom = 2 # number of homology classes; hardcoded for now (we know its 3 for images);
-               # TODO code function to find it
 
-batch_size = 512
+#tf.compat.v1.disable_eager_execution()
+
+man_dim = 2  # Dimension of the manifold
+K = 5  # number of projection bases
+num_of_filtration = 28
+
+num_of_hom = 2  # number of homology classes; hardcoded for now (we know its 3 for images);
+# TODO code function to find it
+max_num_of_pnts = 77
+
+batch_size = 64
 epochs = 3
 save_every = 1
+
 
 def pad_diagrams(dgms):
     '''
@@ -22,6 +29,7 @@ def pad_diagrams(dgms):
     '''
     # Get the highest number of points in a PD
     max_num_of_pnts = 0
+    num_of_dgms = 0
     for ind in dgms.keys():
         num_of_dgms = 0
         for filtration in dgms[ind].keys():
@@ -31,15 +39,19 @@ def pad_diagrams(dgms):
                 num_of_dgms += 1
 
     # Pad
+    # TODO fix this
     max_num_of_pnts = 77
     N = len(dgms.keys())
-    out = np.zeros([N,num_of_dgms,max_num_of_pnts,man_dim+1], dtype=np.float32)
+    out = np.zeros([N, num_of_dgms, num_of_hom, max_num_of_pnts, man_dim], dtype=np.float32)
+    out[:, :, :, 2] = -1
     for ind in dgms.keys():
         cnt = 0
         for filtration in dgms[ind].keys():
             for par_ind in dgms[ind][filtration].keys():
                 dgm = dgms[ind][filtration][par_ind]
-                out[ind,cnt,:dgm.shape[0],:] = dgm
+                for p_ind in range(dgm.shape[0]):
+                    hom = int(dgm[p_ind, 2])
+                    out[ind, cnt, hom, p_ind, :] = dgm[p_ind, :2]
                 cnt += 1
     return out
 
@@ -47,115 +59,52 @@ def pad_diagrams(dgms):
 # Obtain the data
 img_id = 'mnist'
 train_images, train_labels, test_images, test_labels = utils.get_mnist_data()
-train_images = train_images.reshape(train_images.shape[0], 28*28).astype('float32')
-test_images = test_images.reshape(test_images.shape[0], 28*28).astype('float32')
+train_images = train_images.reshape(train_images.shape[0], 28 * 28).astype('float32')
+test_images = test_images.reshape(test_images.shape[0], 28 * 28).astype('float32')
 
 ## Set the params of the filtrations
 # Height filtration
 num_of_vects = 6
-angles = np.linspace(0, math.pi/2, num_of_vects)
-dirs = [[round(math.cos(theta),2),round(math.sin(theta),2)] for theta in angles]
+angles = np.linspace(0, math.pi / 2, num_of_vects)
+dirs = [[round(math.cos(theta), 2), round(math.sin(theta), 2)] for theta in angles]
 dirs = np.array(dirs)
 
 # Radial filtration
-center = np.array([[10,10], [10,20], [20,10], [20,20]])
+center = np.array([[10, 10], [10, 20], [20, 10], [20, 20]])
 radius = np.array([5, 10, 15])
 
 # Erosion filtration
-n_iter_er = np.array([1,2,3,50])
+n_iter_er = np.array([1, 2, 3, 50])
 
 # Dilation filtration
-n_iter_dil = np.array([1,3,5,10,50])
+n_iter_dil = np.array([1, 3, 5, 10, 50])
 
-params = {'cubical' : None,
-         'height': dirs,
-         'radial': {'center' : center,
-                    'radius' : radius
-                    },
-         'erosion': n_iter_er,
-         'dilation': n_iter_dil
-         }
+params = {'cubical': None,
+          'height': dirs,
+          'radial': {'center': center,
+                     'radius': radius
+                     },
+          'erosion': n_iter_er,
+          'dilation': n_iter_dil
+          }
 # Get persistence diagrams
 pd_train = persistence_diagram.PDiagram(train_images, fil_parms=params, images_id='mnist_train')
 pd_test = persistence_diagram.PDiagram(test_images, fil_parms=params, images_id='mnist_test')
 
 # Get train test data
 dgms_train = pd_train.get_pds()
-print(len(dgms_train.keys()))
 dgms_test = pd_test.get_pds()
 x_train = pad_diagrams(dgms_train)
 x_test = pad_diagrams(dgms_test)
 y_train = train_labels
 y_test = test_labels
-# x_train = x_train.reshape(60000,77*28*3)
-# x_test = x_test.reshape(10000,77*28*3)
-#
-# Create TF dataset
-train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
-
-test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
-test_dataset = test_dataset.shuffle(buffer_size=1024).batch(batch_size)
+x_train = np.split(x_train, indices_or_sections= 28, axis=1)
+x_test = np.split(x_test, indices_or_sections= 28, axis=1)
+x_train = [np.squeeze(_) for _ in x_train]
+x_test = [np.squeeze(_) for _ in x_test]
 
 # Set up model
-per_model = layers.PManifoldModel(K, man_dim, num_of_hom)
+in_shape = [num_of_filtration, max_num_of_pnts, 3]
+per_model = layers.PManifoldModel(input_shape=in_shape, num_of_hom=num_of_hom, K=K)
 
-# Instantiate an optimizer and loss
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-
-# Instantiate performance metrics
-train_loss = tf.keras.metrics.Mean(name='train_loss')
-train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
-test_loss = tf.keras.metrics.Mean(name='test_loss')
-test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
-
-# Set up logs
-current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-train_log_dir = 'logs/' + img_id + '/' + current_time + '/train'
-test_log_dir = 'logs/' + img_id + '/' + current_time + '/test'
-train_summary_writer = tf.summary.create_file_writer(train_log_dir)
-test_summary_writer = tf.summary.create_file_writer(test_log_dir)
-
-# # Set up checkpoints
-# checkpoint_directory = ".tmp/training_checkpoints/" + img_id
-# checkpoint = tf.train.Checkpoint(step=tf.Variable(1),
-#                                  optimizer=optimizer, model=per_model)
-# manager = tf.train.CheckpointManager(checkpoint, checkpoint_directory, max_to_keep=3)
-
-# Load weights if any
-# checkpoint.restore(manager.latest_checkpoint)
-# if manager.latest_checkpoint:
-#     print("Restored from {}".format(manager.latest_checkpoint))
-# else:
-#     print("Training from scratch.")
-
-# inputs = tf.keras.Input(shape=(28*77*3,), name='digits')
-# x = tf.keras.layers.Dense(256, activation='relu', name='dense_1')(inputs)
-# x = tf.keras.layers.Dense(256, activation='relu')(x)
-# x = tf.keras.layers.Dense(128, activation='relu', name='dense_2')(x)
-# outputs = tf.keras.layers.Dense(10, name='predictions')(x)
-#
-# model = tf.keras.Model(inputs=[inputs], outputs=outputs)
-
-per_model.compile(optimizer=tf.keras.optimizers.Adam(),  # Optimizer
-               # Loss function to minimize
-               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-               # List of metrics to monitor
-               metrics=['sparse_categorical_accuracy']
-               )
-#
-# print('# Fit model on training data')
-history = per_model.fit(x_train, y_train,
-                    batch_size=64,
-                    epochs=10,
-                    validation_data=(x_test,y_test)
-                    )
-#
-# print('\nhistory dict:', history.history)
-
-# Train and test
-
-
-
-
+per_model.train(x_train, y_train, x_test, y_test)
--- utils.py
+++ utils.py
@@ -11,64 +11,61 @@ class Poincare:
     EPS = 1e-5
     MAX_TANH_ARG = 15.0
 
-    @staticmethod
-    def tf_dot(x, y):
+    def tf_dot(self, x, y):
         return tf.reduce_sum(x * y, keepdims=True)
 
-    @staticmethod
-    def tf_norm(x):
+    def tf_norm(self, x):
         return tf.norm(x, keepdims=True)
 
-    @staticmethod
-    def tf_lambda_x(x, c):
-        return 2. / (1 - c * Poincare.tf_dot(x,x))
 
-    @staticmethod
-    def tf_atanh(x):
-        return tf.atanh(tf.minimum(x, 1. - Poincare.EPS)) # Only works for positive real x.
+    def tf_lambda_x(self, x, c):
+        return 2. / (1 - c * self.tf_dot(x,x))
+
+    def tf_atanh(self, x):
+        return tf.atanh(tf.minimum(x, 1. - self.EPS)) # Only works for positive real x.
 
     # Real x, not vector!
-    @staticmethod
-    def tf_tanh(x):
-       return tf.tanh(tf.minimum(tf.maximum(x, -Poincare.MAX_TANH_ARG), Poincare.MAX_TANH_ARG))
+    def tf_tanh(self, x):
+       return tf.tanh(tf.minimum(tf.maximum(x, -self.MAX_TANH_ARG), self.MAX_TANH_ARG))
 
-    @staticmethod
-    def tf_project_hyp_vecs(x, c):
+    
+    def tf_project_hyp_vecs(self, x, c):
         # Projection op. Need to make sure hyperbolic embeddings are inside the unit ball.
-        return tf.clip_by_norm(t=x, clip_norm=(1. - Poincare.PROJ_EPS) / tf.sqrt(c), axes=[0])
+        return tf.clip_by_norm(t=x, clip_norm=(1. - self.PROJ_EPS) / tf.sqrt(c), axes=[0])
 
-    @staticmethod
-    def tf_mob_add(u, v, c):
-        v = v + Poincare.EPS
-        tf_dot_u_v = 2. * c * Poincare.tf_dot(u, v)
-        tf_norm_u_sq = c * Poincare.tf_dot(u,u)
-        tf_norm_v_sq = c * Poincare.tf_dot(v,v)
+
+    def tf_mob_add(self, u, v, c):
+        v = v + self.EPS
+        tf_dot_u_v = 2. * c * self.tf_dot(u, v)
+        tf_norm_u_sq = c * self.tf_dot(u,u)
+        tf_norm_v_sq = c * self.tf_dot(v,v)
         denominator = 1. + tf_dot_u_v + tf_norm_v_sq * tf_norm_u_sq
         result = (1. + tf_dot_u_v + tf_norm_v_sq) / denominator * u + (1. - tf_norm_u_sq) / denominator * v
-        return Poincare.tf_project_hyp_vecs(result, c)
+        return self.tf_project_hyp_vecs(result, c)
 
 
-    @staticmethod
-    def tf_exp_map_x(x, v, c):
-        v = v + Poincare.EPS # Perturbe v to avoid dealing with v = 0
-        norm_v = Poincare.tf_norm(v)
-        second_term = (Poincare.tf_tanh(tf.sqrt(c) * Poincare.tf_lambda_x(x, c) * norm_v / 2) / (tf.sqrt(c) * norm_v)) * v
-        return Poincare.tf_mob_add(x, second_term, c)
-
-    @staticmethod
-    def tf_log_map_x(x, y, c):
-        diff = Poincare.tf_mob_add(-x, y, c) + Poincare.EPS
-        norm_diff = Poincare.tf_norm(diff)
-        lam = Poincare.tf_lambda_x(x, c)
-        return (((2. / tf.sqrt(c)) / lam) * Poincare.tf_atanh(tf.sqrt(c) * norm_diff) / norm_diff) * diff
+    def tf_exp_map_x(self, x, v, c):
+        v = v + self.EPS # Perturbe v to avoid dealing with v = 0
+        norm_v = self.tf_norm(v)
+        second_term = (self.tf_tanh(tf.sqrt(c) * self.tf_lambda_x(x, c) * norm_v / 2) / (tf.sqrt(c) * norm_v)) * v
+        return self.tf_mob_add(x, second_term, c)
 
 
-    @staticmethod
-    def tf_parametrization(y, theta):
+    def tf_log_map_x(self, x, y, c):
+        diff = self.tf_mob_add(-x, y, c) + self.EPS
+        norm_diff = self.tf_norm(diff)
+        lam = self.tf_lambda_x(x, c)
+        val = (((2. / tf.sqrt(c)) / lam) * self.tf_atanh(tf.sqrt(c) * norm_diff) / norm_diff) * diff
+        return val
+
+
+
+    def tf_parametrization(self, y, theta, m):
         '''
             Transforms the Euclidean point y onto the manifold given params theta
         '''
-        m = y.shape[0] # manifold dim
+        if tf.norm(y) == 0:
+            return tf.zeros(shape=(m,))
         x = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
         for i in tf.range(m, dtype=tf.int32):
             if i == 0:
@@ -85,15 +82,15 @@ class Poincare:
         return x.stack()
 
 
-    @staticmethod
-    def tf_chart(x):
+    def tf_chart(self, x, m):
         '''
             Transforms the manifold point x to the Euclidean space
         '''
-        m = x.shape[0]
+
+
         y = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
         y_0 = tf.multiply(x[0], tf.cos(x[1]))
-        y.write(0,y_0)
+        y = y.write(0,y_0)
         for i in tf.range(1, m, dtype=tf.int32):
             y_i = x[0]
             if i == m-1:
@@ -130,7 +127,7 @@ def get_mnist_data(binirize = False):
     return train_images, train_labels, test_images, test_labels
 
 
-### Poincare model helper functions
+### self.model helper functions
 
 
 
