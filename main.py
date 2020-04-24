12fb42889534e07d8ff234262b92370eb92c8206
pkyriakis <pvkiriakis@gmail.com>
2020-04-24 02:35:17 -0700

    merged into master

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import math
 import os
 import utils
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 from persistence_diagram import *
 from sklearn.model_selection import train_test_split
 
 from train import train
+
+
 def get_data_images(images_id, directions, center, radius, n_iter_er, n_iter_dil):
     '''
         Obtains train/test data for the given image set using the provided filtration paramers
@@ -23,7 +25,7 @@ def get_data_images(images_id, directions, center, radius, n_iter_er, n_iter_dil
         train_images, train_labels, test_images, test_labels = utils.get_cifar()
     elif images_id == 'mpeg7':
         train_images, train_labels, test_images, test_labels = utils.get_mpeg_data()
-    else: # Load mnist by default
+    else:  # Load mnist by default
         train_images, train_labels, test_images, test_labels = utils.get_mnist_data()
 
     # Set filtration params
@@ -56,6 +58,7 @@ def get_data_images(images_id, directions, center, radius, n_iter_er, n_iter_dil
 
     return x_train, y_train, x_test, y_test
 
+
 def get_data_graphs(graphs_id):
     '''
         Obtains train/test data for the given graph dataset
@@ -74,29 +77,29 @@ directions = [[round(math.cos(theta), 3), round(math.sin(theta), 3)] for theta i
 directions = np.array(directions)
 
 # Radial filtration
-center = np.array([[10, 10], [10, 20], [15,15], [20, 10], [20, 20]])
+center = np.array([[10, 10], [10, 20], [15, 15], [20, 10], [20, 20]])
 radius = np.array([5, 8, 10, 12, 15])
 center = np.array([])
 radius = np.array([])
 
 # Erosion filtration
 n_iter_er = np.array([1, 2, 3, 50])
-n_iter_er = np.array([])
+#n_iter_er = np.array([])
 
 # Dilation filtration
 n_iter_dil = np.array([1, 3, 5, 10, 50])
-n_iter_dil = np.array([])
-
+#n_iter_dil = np.array([])
 
 images_id = 'mnist'
 
-train_params = {'units': [128,64,10],
-                'epochs': 20,
-                'batch_size': 64
+train_params = {'units': [128, 64, 10],
+                'epochs': 100,
+                'batch_size': 16
                 }
 # x_train, y_train, x_test, y_test =\
 #     get_data_images(images_id, directions, center, radius, n_iter_er, n_iter_dil)
 
 graphs_id = 'IMDB_BINARY'
 x_train, x_test, y_train, y_test = get_data_graphs(graphs_id)
-train(x_train, y_train, x_test, y_test, man_dim=4, K=20, train_params=train_params)
+
+train(x_train, y_train, x_test, y_test, man_dim=7, K=20, train_params=train_params)
