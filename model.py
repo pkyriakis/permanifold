297e982b6df17cd00ffadb6a11c4644abb59dae3
pkyriakis <pvkiriakis@gmail.com>
2020-04-29 17:48:32 -0700

    updated model to include bacth norm after pmanifold

--- model.py
+++ model.py
@@ -43,24 +43,30 @@ def build_model(input_shape, hparams, units=None):
         inputs.append(cur_input)
         in_layer.append(pm_layer(cur_input))
 
-    # Flatten
+    # Concat
     in_layer_2 = tf.concat(in_layer, axis=1)
 
+    # Flatten
     flat = tf.keras.layers.Flatten()(in_layer_2)
+
+    # Batch norm; really needed cuz the outputs of PManifold layer are essentially sums of points
+    # in a m-dim manifold, they have realy high vals
+    batch_norm = tf.keras.layers.BatchNormalization()(flat)
 
     # First dense
     dense1 = tf.keras.layers.Dense(units[0],
                                         activation='relu')(flat)
 
-    # Batch norm
-    batch_norm = tf.keras.layers.BatchNormalization()(dense1)
-
-    # Second dense
-    dense2 = tf.keras.layers.Dense(units[1],
-                                        activation='relu')(dense1)
+    # Check if there's a second dense
+    if units[1] != 0:
+        # Second dense
+        dense2 = tf.keras.layers.Dense(units[1],
+                                            activation='relu')(dense1)
+    else:
+        dense2 = dense1
 
     # Dropout
-    dropout = tf.keras.layers.Dropout(0.1)(dense2)
+    dropout = tf.keras.layers.Dropout(0.2)(dense2)
 
     # Out
     out_layer = tf.keras.layers.Dense(units=units[2])(dropout)
